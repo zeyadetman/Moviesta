c8c3692d2ed2fe9a9c252a35a0529069d72bd5c5
zeyadetman <zeyadetman@yahoo.com>
2019-01-01 05:41:25 +0200

    Updates

---
+++ precache-manifest.93dd2297acdda0648611d2cf8e8aa492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a8efae5d13a2946c1694",
+    "url": "/Moviesta/static/js/runtime~main.a8efae5d.js"
+  },
+  {
+    "revision": "d977d30b554b15a8cd9b",
+    "url": "/Moviesta/static/js/main.d977d30b.chunk.js"
+  },
+  {
+    "revision": "54b6cb75b8a47d0fe1a6",
+    "url": "/Moviesta/static/js/1.54b6cb75.chunk.js"
+  },
+  {
+    "revision": "d977d30b554b15a8cd9b",
+    "url": "/Moviesta/static/css/main.3a9f1491.chunk.css"
+  },
+  {
+    "revision": "54b6cb75b8a47d0fe1a6",
+    "url": "/Moviesta/static/css/1.791a1592.chunk.css"
+  },
+  {
+    "revision": "d66527b53081e3fe1a8922e8cbb35931",
+    "url": "/Moviesta/index.html"
+  }
+];
